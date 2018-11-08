2a161a197ae3b5ec271e6222777522080f01df34
Freddy Hernandez <freddy.hernandez@fandm.edu>
2018-11-08 18:39:32 -0500

    added function in js to print pokeman objects to the dom + manipulation of the html modals

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-//defining pokemon parent class
+//Array containing all pokeman objects
 
 let allPokemon = [];
+
+
+//Defining trainer class with methods
 
 class Trainer {
   constructor(name){
@@ -26,6 +29,8 @@ class Trainer {
 
 let trainer = new Trainer ('Young Master Duck');
 
+//Defining pokemon class
+
 class Pokemon {
         contructor(name, hp, atk, def, abil) {
             this.name = name;
@@ -37,7 +42,7 @@ class Pokemon {
 }
 
 
-//requesting data on Pichu
+//Requesting data on Pichu
 
 function getPichu() {
     var xhttp = new XMLHttpRequest();
@@ -45,24 +50,16 @@ function getPichu() {
         if (this.readyState == 4 && this.status == 200) {
         data = JSON.parse(this.responseText);
         // console.log(data);
-        let pokeInfo = {
-          name: data.name,
+        let pichu = {
             hp: data.stats[5].base_stat,
            atk: data.stats[4].base_stat,
            def: data.stats[3].base_stat,
          abil1: data.abilities[0].ability.name,
          abil2: data.abilities[1].ability.name
         }
-          console.log(pokeInfo);
-          allPokemon.push(pokeInfo);
-
-          document.getElementById('pichu-name').innerHTML = 'POKEMON NAME: ' + pokeInfo.name.toUpperCase();
-          document.getElementById('pichu-hp').innerHTML = 'HP: ' + pokeInfo.hp;
-          document.getElementById('pichu-atk').innerHTML = 'ATTACK: ' + pokeInfo.atk;
-          document.getElementById('pichu-def').innerHTML = 'DEFENSE: ' + pokeInfo.def;
-          document.getElementById('pichu-abil').innerHTML = 'ABILITIES: ' + pokeInfo.abil1.toUpperCase() + ', ' + pokeInfo.abil2.toUpperCase();
-          document.getElementById('pichu-img').src = 'pichu-block.png'; //accessing pokemon image
-      }
+          // console.log(pokeInfo);
+          allPokemon.push(pichu);
+          printToDom(pichu);
 
     };
     xhttp.open("GET", "http://fizal.me/pokeapi/api/v2/name/pichu.json", true);
@@ -70,20 +67,7 @@ function getPichu() {
 }
 
 
-//hides and reveals block with pichu image
-
-function showPichu() {
-  var x = document.getElementById('pichu-img');
-  if (x.style.display === 'none') {
-    x.style.display = 'block';
-  } else {
-    x.style.display = 'none';
-  }
-}
-
-
-//requesting data on Lugia
-
+//Requesting data on Lugia
 
 function getLugia() {
     var xhttp = new XMLHttpRequest();
@@ -91,42 +75,7 @@ function getLugia() {
         if (this.readyState == 4 && this.status == 200) {
         data = JSON.parse(this.responseText);
         // console.log(data);
-        let pokeInfo = {
-          name: data.name,
-            hp: data.stats[5].base_stat,
-           atk: data.stats[4].base_stat,
-           def: data.stats[3].base_stat,
-           abil1: data.abilities[0].ability.name,
-           abil2: data.abilities[1].ability.name
-        }
-        console.log(pokeInfo);
-        allPokemon.push(pokeInfo);
-
-        document.getElementById('lugia-name').innerHTML = 'POKEMON NAME: ' + pokeInfo.name.toUpperCase();
-        document.getElementById('lugia-hp').innerHTML = 'HP: ' + pokeInfo.hp;
-        document.getElementById('lugia-atk').innerHTML = 'ATTACK: ' + pokeInfo.atk;
-        document.getElementById('lugia-def').innerHTML = 'DEFENSE: ' + pokeInfo.def;
-        document.getElementById('lugia-abil').innerHTML = 'ABILITIES: ' + pokeInfo.abil1.toUpperCase() + ', ' + pokeInfo.abil2.toUpperCase();
-        document.getElementById('lugia-img').src = 'pichu-block.png'; //accessing pokemon image
-      }
-    };
-    xhttp.open("GET", "http://fizal.me/pokeapi/api/v2/name/lugia.json", true);
-    xhttp.send();
-}
-
-
-
-//get data on Entei
-
-
-function getEntei() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-        data = JSON.parse(this.responseText);
-        // console.log(data);
-        let pokeInfo = {
-          name: data.name,
+        let lugia = {
             hp: data.stats[5].base_stat,
            atk: data.stats[4].base_stat,
            def: data.stats[3].base_stat,
@@ -134,25 +83,34 @@ function getEntei() {
            abil2: data.abilities[1].ability.name
         }
         // console.log(pokeInfo);
-        allPokemon.push(pokeInfo);
+        allPokemon.push(lugia);
+        printToDom(lugia);
 
-        // i = 0;
-        // for (prop in pokeInfo) {
-        //   console.log(pokeInfo.name, pokeInfo.hp, pokeInfo.atk, pokeInfo.def, pokeInfo.abil1);
-        //   i++;
-        //   if (i > 5) {
-        //     console.log('');
-        //   }
-        // }
+      }
+    };
+    xhttp.open("GET", "http://fizal.me/pokeapi/api/v2/name/lugia.json", true);
+    xhttp.send();
+}
 
-        // writeToDom(pokeInfo[pokeInfo]);
 
-        // document.getElementById('entei-name').innerHTML = 'POKEMON NAME: ' + pokeInfo.name.toUpperCase();
-        // document.getElementById('entei-hp').innerHTML = 'HP: ' + pokeInfo.hp;
-        // document.getElementById('entei-atk').innerHTML = 'ATTACK: ' + pokeInfo.atk;
-        // document.getElementById('entei-def').innerHTML = 'DEFENSE: ' + pokeInfo.def;
-        // document.getElementById('entei-abil').innerHTML = 'ABILITIES: ' + pokeInfo.abil1.toUpperCase() + ' , ' + pokeInfo.abil2.toUpperCase();
-        // document.getElementById('entei-img').src = 'pichu-block.png'; //accessing pokemon image
+//Requesting data on Entei
+
+function getEntei() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        data = JSON.parse(this.responseText);
+        // console.log(data);
+        let entei = {
+            hp: data.stats[5].base_stat,
+           atk: data.stats[4].base_stat,
+           def: data.stats[3].base_stat,
+           abil1: data.abilities[0].ability.name,
+           abil2: data.abilities[1].ability.name
+        }
+        // console.log(pokeInfo);
+        allPokemon.push(entei);
+        printToDom(entei);
 
       }
     };
@@ -160,8 +118,19 @@ function getEntei() {
     xhttp.send();
 }
 
-// function writeToDom(pok) {
-//   let node = document.createElement('div');
-//   node.innerHTML = getEntei();
-//   document.getElementById('block').appendChild(node);
-// }
+//Prints pokemon attribute to the html screen
+
+function printToDom(pokemon) {
+  var pokeHp = document.getElementById('pokeHp');
+  var pokeAtk = document.getElementById('pokeAtk');
+  var pokeDef = document.getElementById('pokeDef');
+  var pokeAbil1 = document.getElementById('pokeAbil1');
+  var pokeAbil2 = document.getElementById('pokeAbil2');
+
+  pokeHp.innerHTML = "HP: " + pokemon.hp;
+  pokeAtk.innerHTML = "Attack: " + pokemon.atk;
+  pokeDef.innerHTML = "Defense: " + pokemon.def;
+  pokeAbil1.innerHTML = "Abilities: " + pokemon.abil1.toUpperCase() + ", ";
+  pokeAbil2.innerHTML = pokemon.abil2.toUpperCase();
+
+}
